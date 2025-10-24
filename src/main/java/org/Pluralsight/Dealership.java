a4196b53b59cd44bf67ba59d14525c98d2215423
MarcSurpris <MarcSurpris7@gmail.com>
2025-10-24 14:08:40 -0400

    Five commit

---
+++ src/main/java/org/Pluralsight/Dealership.java
@@ -0,0 +1,11 @@
+package org.Pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+}
