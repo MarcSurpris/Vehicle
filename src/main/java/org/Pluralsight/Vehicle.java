d74bd97f21dfdcd0b6c09a0b471239d93007eb63
MarcSurpris <MarcSurpris7@gmail.com>
2025-10-24 11:23:44 -0400

    second commit

--- src/main/java/org/Pluralsight/Vehicle.java
+++ src/main/java/org/Pluralsight/Vehicle.java
@@ -6,5 +6,10 @@ import java.util.List;
 public class Vehicle {
     private int vin;
     private int year;
-
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
 }
