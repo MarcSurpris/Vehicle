e2f3bf47ec92eaf38ac6d6243ca93808d75c536f
MarcSurpris <MarcSurpris7@gmail.com>
2025-10-29 16:17:45 -0400

    13th commit

--- src/main/java/org/Pluralsight/DealershipFileManager.java
+++ src/main/java/org/Pluralsight/DealershipFileManager.java
@@ -19,8 +19,41 @@ public class DealershipFileManager {
                 if (parts.length >= 3) {
                     String name = parts[0];
                     String address = parts[1];
+                    String phone = parts[2];
+                    dealership = new Dealership(name, address, phone);
                 }
             }
+            while ((line = br.readLine() != null)) {
+                String[] parts = line.split("\\|");
+                if (parts.length >= 8 && dealership != null) {
+                    int vin = Integer.parseInt(parts[0]);
+                    int year = Integer.parseInt(parts[1]);
+                    String make = parts[2];
+                    String model = parts[3];
+                    String vehicleType = parts[4];
+                    String color = parts[5];
+                    int odometer = Integer.parseInt(parts[6]);
+                    double price = Double.parseDouble(parts[7]);
+                    Vehicle vehicle = new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
+                    dealership.addVehicle(vehicle);
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return dealership;
+    }
+
+    public void saveDealership(Dealership dealership) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_NAME))) {
+            bw.write(dealership.getName() + "|" + dealership.getAddress() + "|" + dealership.getPhone());
+            bw.newLine();
+            for (Vehicle vehicle : dealership.getInventory()) {
+                bw.write(vehicle.toString());
+                bw.newLine();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ src/main/java/org/Pluralsight/UserInterface.java
@@ -0,0 +1,70 @@
+package org.Pluralsight;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class UserInterface {
+    private Dealership dealership;
+    private Scanner scanner = new Scanner(System.in);
+
+    public void display() {
+        init();
+        if (dealership == null) {
+            System.out.println("Error loading dealership data.");
+            return;
+        }
+        boolean running = true;
+        while (running) {
+            displayMenu();
+            String command = scanner.nextLine().trim();
+            switch (command) {
+                case "1":
+                    processGetByPriceRequest();
+                    break;
+                case "2":
+                    processGetByMakeModelRequest();
+                    break;
+                case "3":
+                    processGetByYearRequest();
+                    break;
+                case "4":
+                    processGetByColorRequest();
+                    break;
+                case "5":
+                    processGetByMileageRequest();
+                    break;
+                case "6":
+                    processGetByVehicleTypeRequest();
+                    break;
+                case "7":
+                    processAllVehiclesRequest();
+                    break;
+                case "8":
+                    processAddVehicleRequest();
+                    break;
+                case "9":
+                    processRemoveVehicleRequest();
+                    break;
+                case "99":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please Try again");
+            }
+        }
+        scanner.close();
+    }
+
+    private void init() {
+        DealershipFileManager fileManager = new DealershipFileManager();
+        this.dealership = fileManager.getDealership();
+    }
+
+    private void displayMenu() {
+        System.out.println("\nMenu:");
+        System.out.println("1 - Find vehicles within a price range");
+        System.out.println("2 - Find vehicles by make / model");
+        System.out.println("3 - Find vehicles by year range");
+        System.out.println("");
+    }
+}
